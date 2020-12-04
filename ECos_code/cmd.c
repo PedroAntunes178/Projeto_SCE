a7c76f3966310506d16bf43f9d27efd584a3eec6
Pedro Antunes <pedronmantunes@gmail.com>
2020-12-04 23:30:16 +0000

    fixe infinite loop

--- ECos_code/cmd.c
+++ ECos_code/cmd.c
@@ -56,6 +56,9 @@ void cyg_user_start(void){
 void cmd_program(cyg_addrword_t data){
   int delay=10;
 
+  cyg_mutex_lock(&cliblock)
+  printf("%s Type sos for help\n", TitleMsg);
+  cyg_mutex_unlock(&cliblock);
   while(1){
     cyg_mutex_lock(&cliblock);
     monitor();
--- ECos_code/monitor.c
+++ ECos_code/monitor.c
@@ -105,20 +105,17 @@ void monitor (void)
   static char *argv[ARGVECSIZE+1], *p;
   int argc, i;
 
-  printf("%s Type sos for help\n", TitleMsg);
-  for (;;) {
-    printf("\nMyCmd> ");
-    /* Reading and parsing command line  ----------------------------------*/
-    if ((argc = my_getline(argv, ARGVECSIZE)) > 0) {
-      for (p=argv[0]; *p != '\0'; *p=tolower(*p), p++);
-      for (i = 0; i < NCOMMANDS; i++)
-	if (strcmp(argv[0], commands[i].cmd_name) == 0)
-	  break;
-      /* Executing commands -----------------------------------------------*/
-      if (i < NCOMMANDS)
-	commands[i].cmd_fnct (argc, argv);
-      else
-	printf("%s", InvalMsg);
-    } /* if my_getline */
-  } /* forever */
+  printf("\nMyCmd> ");
+  /* Reading and parsing command line  ----------------------------------*/
+  if ((argc = my_getline(argv, ARGVECSIZE)) > 0) {
+    for (p=argv[0]; *p != '\0'; *p=tolower(*p), p++);
+    for (i = 0; i < NCOMMANDS; i++)
+      if (strcmp(argv[0], commands[i].cmd_name) == 0)
+        break;
+    /* Executing commands -----------------------------------------------*/
+    if (i < NCOMMANDS)
+      commands[i].cmd_fnct (argc, argv);
+    else
+      printf("%s", InvalMsg);
+  } /* if my_getline */
 }
