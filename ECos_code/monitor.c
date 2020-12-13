fe77f22a82fcbc94ca65144e8fca4eb7f17fabf3
Pedro Antunes <pedronmantunes@gmail.com>
2020-12-13 19:00:19 +0000

    added to cmd monitor all commands

--- ECos_code/monitor.c
+++ ECos_code/monitor.c
@@ -34,8 +34,14 @@ struct 	command_d {
   {cmd_ir,   "ir","                              information about registers (NREG, nr, iread, iwrite)"},
   {cmd_trc,  "trc","<n>                          transfer n registers from current iread position"},
   {cmd_tri,  "tri","<n> <i>                      transfer n registers from index i (0 - oldest)"},
-  //...
-  {cmd_pr,   "pr","<[h1 m1 s1]> <[h2 m2 s2]>     process registers (max, min, mean) between instants t1 and t2 (h,m,s)"}
+  {cmd_irl,  "irl","                             information about local registers (NRBUF, nr, iread, iwrite)"},
+  {cmd_lr,   "lr","<n> <i>                       list n registers (local memory) from index i (0 - oldest)"},
+  {cmd_dr,   "dr","                              delete registers (local memory)"},
+  {cmd_irl,  "irl","                             information about local registers (NRBUF, nr, iread, iwrite)"},
+  {cmd_mpt,  "mpt","<p>                          modify period of transference (minutes - 0 deactivate)"},
+  {cmd_cttl, "cttl","                            check threshold temperature and luminosity for processing"},
+  {cmd_dttl, "dttl","                            define threshold temperature and luminosity for processing"},
+  {cmd_pr,   "pr","<[h1 m1 s1 [h2 m2 s2]]>       process registers (max, min, mean) between instants t1 and t2 (h,m,s)"}
 };
 
 #define NCOMMANDS  (sizeof(commands)/sizeof(struct command_d))
